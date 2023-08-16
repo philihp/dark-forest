00206458270e45fc10c6c3394c1aabfdaaf7734c
Philihp Busby <philihp@gmail.com>
2023-08-16 01:58:19 +0000

    feature: show spiral galaxy

--- client/web/src/pages/Game.tsx
+++ client/web/src/pages/Game.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { range } from 'ramda'
 import { HeaderUser } from '../components/HeaderUser'
 import { Loading } from '../components/Loading'
 
@@ -10,10 +11,29 @@ const Game = () => {
   const { connecting, state } = useHathoraContext()
   useAutoConnect(gameId)
 
+  const nodes = range(0, 100)
+
   return (
     <>
       <HeaderUser />
       {connecting && <Loading />}
+      <svg
+        viewBox="-100 -100 200 200" /* min-x min-y width height */
+        preserveAspectRatio="xMidYMid meet"
+        style={{ width: '100%', height: '40em' }}
+        role="img"
+      >
+        <title>A gradient</title>
+        {nodes.map((n) => {
+          const baseAngle = Math.PI * (1 + Math.sqrt(5))
+          const r = Math.sqrt(n + 0.5) * 7
+          const theta = n * baseAngle
+          const x = r * Math.cos(theta)
+          const y = r * Math.sin(theta)
+
+          return <circle key={`dot${n}`} cx={`${x}`} cy={`${y}`} r="2" style={{ fill: '#87a74f' }} />
+        })}
+      </svg>
       <pre>{JSON.stringify(state, undefined, 2)}</pre>
     </>
   )
--- client/web/src/pages/Home.tsx
+++ client/web/src/pages/Home.tsx
@@ -19,71 +19,67 @@ const Home = () => {
     <>
       <HeaderUser />
       <h1>Dark Forest</h1>
-      <p>
-        <h3>Public Servers</h3>
-        <table border={1} cellPadding={3} cellSpacing={0}>
-          <thead>
-            <tr>
-              <th>Room Code</th>
-              <th>Region</th>
-              <th colSpan={3}>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lobbies &&
-              lobbies.map((lobby) => {
-                const { roomId } = lobby
-                const { region } = lobby
-                const state = (lobby as unknown as { state: { players?: number; country?: string } })?.state
-                return (
-                  <tr>
-                    <td>{roomId}</td>
-                    <td>{region}</td>
-                    <td>
-                      {state?.players} players
-                      <br />
-                      {state?.country}
-                    </td>
-                    <td>{!state?.players && !state?.country && JSON.stringify({ state })}</td>
-                    <td>
-                      <button
-                        type="button"
-                        onClick={async () => {
-                          navigate(`/game/${roomId}`)
-                        }}
-                      >
-                        Join
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })}
-          </tbody>
-        </table>
-        <button
-          disabled={!user}
-          type="button"
-          onClick={async () => {
-            const roomId = await createPublicLobby()
-            navigate(`/game/${roomId}`)
-          }}
-        >
-          Create Public Server
-        </button>
-      </p>
-      <p>
-        <h3>Private Servers</h3>
-        <button
-          disabled={!user}
-          type="button"
-          onClick={async () => {
-            const roomId = await createPrivateLobby()
-            navigate(`/game/${roomId}`)
-          }}
-        >
-          Create Private Lobby
-        </button>
-      </p>
+      <h3>Public Servers</h3>
+      <table border={1} cellPadding={3} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>Room Code</th>
+            <th>Region</th>
+            <th colSpan={3}>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lobbies &&
+            lobbies.map((lobby) => {
+              const { roomId } = lobby
+              const { region } = lobby
+              const state = (lobby as unknown as { state: { players?: number; country?: string } })?.state
+              return (
+                <tr>
+                  <td>{roomId}</td>
+                  <td>{region}</td>
+                  <td>
+                    {state?.players} players
+                    <br />
+                    {state?.country}
+                  </td>
+                  <td>{!state?.players && !state?.country && JSON.stringify({ state })}</td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={async () => {
+                        navigate(`/game/${roomId}`)
+                      }}
+                    >
+                      Join
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </table>
+      <button
+        disabled={!user}
+        type="button"
+        onClick={async () => {
+          const roomId = await createPublicLobby()
+          navigate(`/game/${roomId}`)
+        }}
+      >
+        Create Public Server
+      </button>
+      <h3>Private Servers</h3>
+      <button
+        disabled={!user}
+        type="button"
+        onClick={async () => {
+          const roomId = await createPrivateLobby()
+          navigate(`/game/${roomId}`)
+        }}
+      >
+        Create Private Lobby
+      </button>
       <hr />
       Dark Forest, made with ♥ in San Francisco.
     </>
