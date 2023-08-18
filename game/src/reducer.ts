f4b2803272e784f10e943d0d202e6a39495f85e0
Philihp Busby <philihp@gmail.com>
2023-08-18 03:30:50 +0000

    refactor: move player to before command

--- game/src/reducer.ts
+++ game/src/reducer.ts
@@ -3,9 +3,10 @@ import { always, any } from 'ramda'
 import { GameCommand, GameState, StateReducer } from './types'
 import { start, spawn, transit } from './commands'
 
-export const reducer = ([timeRaw, command, ...params]: string[]): StateReducer => {
+export const reducer = ([timeRaw, playerRaw, command, ...params]: string[]): StateReducer => {
   const time = Number.parseInt(timeRaw, 10)
-  if (Number.isNaN(time)) return always(undefined)
+  const player = Number.parseInt(playerRaw, 10)
+  if (any(Number.isNaN, [time, player])) return always(undefined)
   return (state) =>
     match<[string, string[]], GameState | undefined>([command, params])
       .with([GameCommand.START, P.array(P.string)], ([_, params]) => {
@@ -15,17 +16,15 @@ export const reducer = ([timeRaw, command, ...params]: string[]): StateReducer =
         if (Number.isNaN(seed)) return start({ sols, seed: undefined })(state)
         return start({ sols, seed })(state)
       })
-      .with([GameCommand.SPAWN, [P.string, P.string]], ([_, params]) => {
-        const player = Number.parseInt(params[0], 10)
-        const sol = Number.parseInt(params[1], 10)
-        if (any(Number.isNaN, [time, sol, player])) return undefined
+      .with([GameCommand.SPAWN, [P.string]], ([_, params]) => {
+        const sol = Number.parseInt(params[0], 10)
+        if (Number.isNaN(sol)) return undefined
         return spawn({ time, sol, player })(state)
       })
-      .with([GameCommand.TRANSIT, [P.string, P.string, P.string]], ([_, params]) => {
-        const player = Number.parseInt(params[0], 10)
-        const source = Number.parseInt(params[1], 10)
-        const destination = Number.parseInt(params[2], 10)
-        if (any(Number.isNaN, [time, player, source, destination])) return undefined
+      .with([GameCommand.TRANSIT, [P.string, P.string]], ([_, params]) => {
+        const source = Number.parseInt(params[0], 10)
+        const destination = Number.parseInt(params[1], 10)
+        if (any(Number.isNaN, [source, destination])) return undefined
         return transit({ time, player, source, destination })(state)
       })
       .otherwise(() => undefined)
