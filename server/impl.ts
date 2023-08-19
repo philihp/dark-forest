af7f09415d1dab804d4d8f75ec9a3c4bab7fcbce
Philihp Busby <philihp@gmail.com>
2023-08-19 03:01:25 +0000

    feature: nicer colors

--- server/impl.ts
+++ server/impl.ts
@@ -26,18 +26,14 @@ type InternalState = {
 }
 
 const colors = [
-  '#8dd3c7',
-  '#ffffb3',
-  '#bebada',
-  '#fb8072',
-  '#80b1d3',
-  '#fdb462',
-  '#b3de69',
-  '#fccde5',
-  '#d9d9d9',
-  '#bc80bd',
+  '#fbb4ae',
+  '#b3cde3',
   '#ccebc5',
-  '#ffed6f',
+  '#decbe4',
+  '#fed9a6',
+  '#ffffcc',
+  '#e5d8bd',
+  '#fddaec',
 ]
 
 
